e22fef31d1bd59a501fa16be63f6102967cd7d29
User <User@IRA-NB.lan>
2025-01-10 15:14:26 +0800

    .

--- Scripts/1. Home/New Test Case/Script1736493046196.groovy
+++ Scripts/1. Home/New Test Case/Script1736493046196.groovy
@@ -17,5 +17,9 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('', false)
+Mobile.startApplication('C:\\Users\\User\\Downloads\\Setia_City_Mall_Version_5_13_11_(Version_Code_162).apk', true)
+
+Mobile.tap(findTestObject('Object Repository/SCM/android.view.ViewGroup'), 0)
+
+Mobile.closeApplication()
 
