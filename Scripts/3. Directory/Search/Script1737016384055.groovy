96c6db17de903c569a5b0cf65b3b50da78c0ab70
fshahhusna <fshahhusna@gmail.com>
2025-01-16 16:41:58 +0800

    add

--- Scripts/3. Directory/Search/Script1737016384055.groovy
+++ Scripts/3. Directory/Search/Script1737016384055.groovy
@@ -17,15 +17,3 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-Mobile.startApplication('C:\\Users\\TUF\\Downloads\\Setia_City_Mall_Version_5_13_11_(Version_Code_162).apk', true)
-
-Mobile.tap(findTestObject('Object Repository/SCM/New Folder_test/android.widget.ImageView'), 0)
-
-Mobile.pressBack()
-
-Mobile.tap(findTestObject('Object Repository/SCM/New Folder_test/android.widget.Button - NO'), 0)
-
-Mobile.tap(findTestObject('Object Repository/SCM/New Folder_test/android.widget.ImageView'), 0)
-
-Mobile.closeApplication()
-
